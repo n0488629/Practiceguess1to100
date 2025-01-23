b69c2ed441f90c75ff13916580b7a685ed4ea8cd
Nick <Nick@DESKTOP-82IOVQF>
2025-01-23 10:46:55 -0600

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please guess the number between 1 and 100");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            int guesses = 1;
+
+            while (guess != randomNumber)
+            {
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Please guess a higher number");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    guesses++;
+                }
+                else if (guess > randomNumber) 
+                {
+                
+                    Console.WriteLine("Please guess a lower number");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    guesses++;
+                }
+
+            }
+            Console.WriteLine(guess + " was the correct number, it took you " + guesses + " guesses.");
+            Console.ReadLine();
+        }
+    }
+}
